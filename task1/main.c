75a8a0b846f97e250cb29ce2ab89da158677226d
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-10-02 23:56:37 +0300

    change return value

--- task1/main.c
+++ task1/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "test.h"
+#include "utils.h"
 
 int main() {
     FILE *test_file;
     test_file = fopen(TEST_FILE_PATH, "r");
     if (!test_file) {
         printf("Unable to open file: %s\n", TEST_FILE_PATH);
-        return 1;
+        return UNABLE_TO_OPEN_FILE;
     }
 
     test(test_file);
 
     fclose(test_file);
-    return 0;
+    return DEFAULT;
 }
