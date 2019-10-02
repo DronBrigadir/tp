3edb889954ccf1cca58ef76df51eaca48a7cf567
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-10-02 23:58:28 +0300

    matrix files: function for matrices and struct

--- task1/matrix.c
+++ task1/matrix.c
@@ -2,8 +2,66 @@
 // Created by andrey on 01.10.2019.
 //
 
+#include <string.h>
 #include "matrix.h"
+#include "utils.h"
+#include "malloc.h"
 
-int matrix_multiplication(Matrix *a, Matrix *b, Matrix *result) {
-    return 0;
+int matrix_multiplication(Matrix *a, Matrix *b, Matrix **result) {
+    if (a->num_col != b->num_row) {
+        return MATR_CANNOT_BE_MULTIPLIED;
+    }
+
+    if (init_matr(result, a->num_row, b->num_col) == MEM_ALLOC_ERR) {
+        return MEM_ALLOC_ERR;
+    }
+
+    for (size_t i = 0; i < a->num_row; i++)
+    {
+        for (size_t j = 0; j < b->num_col; j++)
+        {
+            (*result)->data[i][j] = 0;
+            for (size_t k = 0; k < a->num_col; k++)
+                (*result)->data[i][j] += a->data[i][k] * b->data[k][j];
+        }
+    }
+
+    return DEFAULT;
+}
+
+int init_matr(Matrix **matrix, int num_row, int num_col) {
+    (*matrix) = (Matrix*)malloc(sizeof(Matrix));
+    if ((*matrix) == NULL) {
+        return MEM_ALLOC_ERR;
+    }
+    (*matrix)->num_col = num_col;
+    (*matrix)->num_row = num_row;
+
+    (*matrix)->data = (int**)malloc((*matrix)->num_row * sizeof(int*));
+    if ((*matrix)->data == NULL) {
+        free(*matrix);
+        return MEM_ALLOC_ERR;
+    }
+    int is_alloc_err = DEFAULT;
+    for (size_t i = 0; i < (*matrix)->num_row; i++) {
+        (*matrix)->data[i] = (int*)malloc((*matrix)->num_col * sizeof(int));
+        if ((*matrix)->data[i] == NULL) {
+            is_alloc_err = MEM_ALLOC_ERR;
+            break;
+        }
+    }
+    if (is_alloc_err == MEM_ALLOC_ERR) {
+        free_matr(*matrix);
+        return MEM_ALLOC_ERR;
+    }
+
+    return DEFAULT;
+}
+
+void free_matr(Matrix *matrix) {
+    for (size_t i = 0; i < matrix->num_row; i++) {
+        free(matrix->data[i]);
+    }
+    free(matrix->data);
+    free(matrix);
 }
--- task1/matrix.h
+++ task1/matrix.h
@@ -6,13 +6,15 @@
 #define TASK1_MATRIX_H
 
 struct Matrix {
-    int **matr;
-    int x_size;
-    int y_size;
+    int **data;
+    int num_row;
+    int num_col;
 };
 
 typedef struct Matrix Matrix;
 
-int matrix_multiplication(Matrix *a, Matrix *b, Matrix *result);
+int matrix_multiplication(Matrix *a, Matrix *b, Matrix **result);
+int init_matr(Matrix **matrix, int num_row, int num_col);
+void free_matr(Matrix *matrix);
 
 #endif //TASK1_MATRIX_H
