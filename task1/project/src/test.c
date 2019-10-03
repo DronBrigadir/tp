2a18344220a2e7e52ec700ed6ea2201f0152731a
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-10-03 19:22:53 +0300

    changing the structure of the response

--- task1/project/src/test.c
+++ task1/project/src/test.c
@@ -12,57 +12,65 @@ void test(FILE *in) {
     char str[MAX_BUFFER];
 
     while (fgets(str, MAX_BUFFER, in)) {
-        Matrix *a, *b;
-        char *test_name, *output;
+        Matrix *a, *b, *answer;
+        char *test_name;
 
-        int parser_return_value = parser(&test_name, &a, &b, &output, str);
+        int parser_return_value = parser(&test_name, &a, &b, &answer, str);
+
         if (parser_return_value == WRONG_INPUT) {
             printf("Wrong input\n");
             return;
         } else if (parser_return_value == MEM_ALLOC_ERR) {
             printf("Memory allocation error\n");
             return;
-        } else if (parser_return_value == DEFAULT) {
-            Matrix *result_matr;
-            int mult_return_value = matrix_multiplication(a, b, &result_matr);
+        } else {
+            Matrix *result;
+            int mult_return_value = matrix_multiplication(a, b, &result);
+
             free_matr(a);
             free_matr(b);
+
             if (mult_return_value == MEM_ALLOC_ERR) {
+                if (parser_return_value != MATR_CANNOT_BE_MULTIPLIED) {
+                    free_matr(answer);
+                }
                 printf("Memory allocation error\n");
                 return;
-            } else if (mult_return_value == MATR_CANNOT_BE_MULTIPLIED) {
-                if (check_str_output("Matrix_cannot_be_multiplied\n", output) == DEFAULT) {
+            }
+
+            if (parser_return_value == MATR_CANNOT_BE_MULTIPLIED) {
+                if (mult_return_value == MATR_CANNOT_BE_MULTIPLIED) {
                     printf("%s passed\n", test_name);
-                } else {
+                } else if (mult_return_value == DEFAULT) {
                     printf("%s failed\n", test_name);
+                    free_matr(result);
                 }
-            } else if (mult_return_value == DEFAULT) {
-                int answer = check_matr_output(result_matr, output);
-                free_matr(result_matr);
-                if (answer == MEM_ALLOC_ERR) {
-                    printf("Memory allocation error\n");
-                    return;
-                } else if (answer == WRONG_INPUT) {
-                    printf("Wrong input\n");
-                    return;
-                } else if (answer == WRONG_RESULT) {
+            } else if (parser_return_value == DEFAULT) {
+                if (mult_return_value == MATR_CANNOT_BE_MULTIPLIED) {
                     printf("%s failed\n", test_name);
-                } else if (answer == DEFAULT) {
-                    printf("%s passed\n", test_name);
+                } else if (mult_return_value == DEFAULT) {
+                    int equal_return_value = is_matrix_equal(result, answer);
+                    free_matr(result);
+
+                    if (equal_return_value == WRONG_RESULT) {
+                        printf("%s failed\n", test_name);
+                    } else if (equal_return_value == DEFAULT) {
+                        printf("%s passed\n", test_name);
+                    }
                 }
+
+                free_matr(answer);
             }
         }
     }
 
-    if (feof(in) != 0) {
-        printf("Tests passed\n");
-    } else {
+    if (feof(in) == 0) {
         printf("Error reading from file\n");
     }
 }
 
-int parser(char **test_name, Matrix **a, Matrix **b, char **output, char *input) {
-    char *separator = " ";
+int parser(char **test_name, Matrix **a, Matrix **b, Matrix **answer, char *input) {
+    char *separator = " |";
 
     // Получение наименования теста
     (*test_name) = strtok(input, separator);
@@ -135,80 +143,47 @@ int parser(char **test_name, Matrix **a, Matrix **b, char **output, char *input)
         }
     }
 
-    // Получение ответа
-    (*output) = strtok(NULL, separator);
-    if (*output == NULL) {
+    // Получение количества строк матрицы ответа
+    int row_answer;
+    tmp = strtok(NULL, separator);
+    if (str_to_int(tmp, &row_answer) == WRONG_INPUT) {
         free_matr(*a);
         free_matr(*b);
         return WRONG_INPUT;
     }
 
-    return DEFAULT;
-}
-
-int check_str_output (char *result, char *output) {
-    if (strlen(result) != strlen(output)) {
-        return WRONG_RESULT;
-    } else {
-        for (size_t i = 0; i < strlen(result); i++) {
-            if (*result != *output) {
-                return WRONG_RESULT;
-            }
-            result++;
-            output++;
-        }
-
-        return DEFAULT;
-    }
-}
-
-int check_matr_output (Matrix *result, char *output) {
-    char *separator = "|";
-
-    // Получение количества строк для матрицы
-    int row;
-    char *tmp = strtok(output, separator);
-    if (str_to_int(tmp, &row) == WRONG_INPUT) {
-        return WRONG_INPUT;
+    if (row_answer == 0) {
+        return MATR_CANNOT_BE_MULTIPLIED;
     }
 
-    // Получение количества столбцов для матрицы
-    int col;
+    // Получение количества столбцов матрицы ответа
+    int col_answer;
     tmp = strtok(NULL, separator);
-    if (str_to_int(tmp, &col) == WRONG_INPUT) {
+    if (str_to_int(tmp, &col_answer) == WRONG_INPUT) {
+        free_matr(*a);
+        free_matr(*b);
         return WRONG_INPUT;
     }
 
-    // Выделение памяти для матрицы
-    Matrix *answer;
-    if (init_matr(&answer, row, col) == MEM_ALLOC_ERR) {
+    // Выделение памяти для матрицы ответа
+    if (init_matr(answer, row_answer, col_answer) == MEM_ALLOC_ERR) {
+        free_matr(*a);
+        free_matr(*b);
         return MEM_ALLOC_ERR;
     }
 
-    // Получение данных для матрицы
-    for (size_t i = 0; i < answer->num_row; i++) {
-        for (size_t j = 0; j < answer->num_col; j++) {
+    // Получение данных для матрицы ответа
+    for (size_t i = 0; i < (*answer)->num_row; i++) {
+        for (size_t j = 0; j < (*answer)->num_col; j++) {
             tmp = strtok(NULL, separator);
-            if (str_to_int(tmp, &(answer->data[i][j])) == WRONG_INPUT) {
-                free_matr(answer);
+            if (str_to_int(tmp, &(*answer)->data[i][j]) == WRONG_INPUT) {
+                free_matr(*a);
+                free_matr(*b);
+                free_matr(*answer);
                 return WRONG_INPUT;
             }
         }
     }
 
-    if (result->num_row != answer->num_row || result->num_col != answer->num_col) {
-        free_matr(answer);
-        return WRONG_RESULT;
-    }
-    for (size_t i = 0; i < result->num_row; i++) {
-        for (size_t j = 0; j < result->num_col; j++) {
-            if (result->data[i][j] != answer->data[i][j]) {
-                free_matr(answer);
-                return WRONG_RESULT;
-            }
-        }
-    }
-
-    free_matr(answer);
     return DEFAULT;
 }
