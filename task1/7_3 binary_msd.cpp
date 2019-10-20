3d213b2c68f3ebfd1595ecbabbc60fe344fb7fc7
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-10-20 17:56:59 +0300

    home work 1, task 7_3: works, but does not pass from memory

--- task1/7_3 binary_msd.cpp
+++ task1/7_3 binary_msd.cpp
@@ -11,9 +11,9 @@
 template <typename T>
 class Array {
 public:
-    Array () {
+    explicit Array (int size) {
         curSize_ = 0;
-        size_ = 4;
+        size_ = size;
         array_ = new T [size_];
     }
     ~Array() {
@@ -36,12 +36,19 @@ public:
     void DeleteLast() {
         curSize_--;
     }
-    void Print() {
+    void Print(std::ostream &out) {
+        std::stringstream printArray;
         for (size_t i = 0; i < this->GetSize(); i++) {
-            std::cout << array_[i] << ' ';
+            printArray << array_[i] << ' ';
         }
-
-        std::cout << std::endl;
+        std::string tmp = printArray.str();
+        tmp.back() = '\n';
+        out << tmp;
+    }
+    void Swap (int l, int r) {
+        T tmp = array_[l];
+        array_[l] = array_[r];
+        array_[r] = tmp;
     }
 
 private:
@@ -61,25 +68,105 @@ private:
     }
 };
 
-void run(std::istream &in, std::ostream &out) {
+template <typename T>
+bool getBit(const T val, unsigned int bitNum) {
+    return (val >> bitNum) & 1 == 1;
+}
 
+template <typename T>
+void helpBinQuickSort(Array<T> *arr, int l, int r, unsigned int bitNumber) {
+    if (l == r) {
+        return;
+    }
+
+    int i = l;
+    int j = r;
+
+    while (i != j) {
+        while (!getBit((*arr)[i], bitNumber) && i < j) {
+            i++;
+        }
+        while (getBit((*arr)[j], bitNumber) && j > i) {
+            j--;
+        }
+
+        if (i != j) {
+            arr->Swap(i, j);
+        }
+    }
+
+    if (!getBit((*arr)[r], bitNumber) || getBit((*arr)[l], bitNumber)) {
+        helpBinQuickSort(arr, l, r, bitNumber - 1);
+        return;
+    }
+
+    helpBinQuickSort(arr, l, j - 1, bitNumber - 1);
+    helpBinQuickSort(arr, j, r, bitNumber - 1);
+}
+
+template <typename T>
+void binQuickSort(Array<T> *arr, int l, int r) {
+    if (arr->GetSize() < r) {
+        throw std::out_of_range("The right border is larger than the array size");
+    }
+
+    if (l > r) {
+        throw std::invalid_argument("The right border should be larger than the left");
+    }
+
+    if (l == r) {
+        return;
+    }
+
+    unsigned int highBitNumber = sizeof(T) * 8 - 1;
+    helpBinQuickSort(arr, l, r, highBitNumber);
+}
+
+
+
+void run(std::istream &in, std::ostream &out) {
+    size_t n = 0;
+    in >> n;
+    assert(n < 1000000);
+
+    Array<unsigned long long> array = Array<unsigned long long>(n);
+
+    for (size_t i = 0; i < n; i++) {
+        unsigned long long tmp = 0;
+        in >> tmp;
+
+        array.Add(tmp);
+    }
+
+    binQuickSort(&array, 0, array.GetSize() - 1);
+    array.Print(out);
 }
 
 void test() {
     {
         std::stringstream input;
         input << "3" << std::endl;
-        input << "4 1000000 7" << std::endl;
+        input << "4 1000000 7 " << std::endl;
 
         std::stringstream output;
 
         run(input, output);
-        assert(output.str() == "4 7 1000000");
+        assert(output.str() == "4 7 1000000\n");
+    }
+    {
+        std::stringstream input;
+        input << "5" << std::endl;
+        input << "5 3 24 21 19" << std::endl;
+
+        std::stringstream output;
+
+        run(input, output);
+        assert(output.str() == "3 5 19 21 24\n");
     }
 }
 
 int main () {
-    //test();
-    run(std::cin, std::cout);
+    test();
+    //run(std::cin, std::cout);
     return 0;
 }
