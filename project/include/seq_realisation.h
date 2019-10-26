0669e706a2cd385c02c504d3f42c05ac360ac10d
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-10-26 12:53:56 +0300

    use another algo for task

--- project/include/seq_realisation.h
+++ project/include/seq_realisation.h
@@ -1,6 +1,7 @@
 #ifndef TASK_SEQ_REALISATION_H
 #define TASK_SEQ_REALISATION_H
 
-int find_lis(int *src, int size_src, int **dest, int *size_dest);
+int find_lis(int *buf, int buf_size, int **answer, int *answer_size);
+int ceil_index(int *subsequence, int l, int r, int key);
 
 #endif //TASK_SEQ_REALISATION_H
--- project/src/seq_realisation.c
+++ project/src/seq_realisation.c
@@ -1,71 +1,121 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "seq_realisation.h"
 #include "utils.h"
 
-int find_lis(int *src, int size_src, int **dest, int *size_dest) {
-    if (!src) {
+int find_lis(int *buf, int buf_size, int **answer, int *answer_size) {
+    if (buf_size <= 1) {
+        return buf_size;
+    }
+
+    if (!buf) {
         return NULL_PTR;
     }
 
-    int *prev = (int*)malloc(size_src * sizeof(int));
-    if (!prev) {
+    int lis_length = -1;
+
+    int *subsequence = (int*)malloc(buf_size * sizeof(int));
+    if (!subsequence) {
         return ERR_MEM_ALLOC;
     }
 
-    int *d = (int*)malloc(size_src * sizeof(int));
-    if (!d) {
-        free(prev);
+    int *indexes = (int*)malloc(buf_size * sizeof(int));
+    if (!indexes) {
+        free(subsequence);
         return ERR_MEM_ALLOC;
     }
 
-    for (int i = 0; i < size_src; i++) {
-        d[i] = 1;
-        prev[i] = -1;
-        for (int j = 0; j < i; j++) {
-            if (src[j] < src[i] && d[j] + 1 > d[i]) {
-                d[i] = d[j] + 1;
-                prev[i] = j;
+    for (int i = 0; i < buf_size; i++) {
+        subsequence[i] = INT_MAX;
+        subsequence[i] = INT_MAX;
+    }
+
+    subsequence[0] = buf[0];
+    indexes[0] = 0;
+
+    for (int i = 1; i < buf_size; i++) {
+        indexes[i] = ceil_index(subsequence, 0, i, buf[i]);
+
+        if (lis_length < indexes[i]) {
+            lis_length = indexes[i];
+        }
+    }
+
+    (*answer_size) = lis_length + 1;
+
+    (*answer) = (int*)malloc ((*answer_size) * sizeof(int));
+    if (!(*answer)) {
+        free(subsequence);
+        free(indexes);
+        return ERR_MEM_ALLOC;
+    }
+
+    int pos = 0;
+    while (indexes[pos] != lis_length) {
+        pos++;
+    }
+
+    int j = ((*answer_size) - 1);
+    (*answer)[j--] = buf[pos];
+
+    int tmp = indexes[pos];
+    if (tmp != 0) {
+        for (int i = pos - 1; i >= 0; i--) {
+            if (indexes[i] == (tmp - 1)) {
+                (*answer)[j--] = buf[i];
+                tmp--;
             }
         }
     }
 
-    int pos = 0;
-    int length = d[0];
-    for (int i = 0; i < size_src; i++) {
-        if (d[i] > length) {
-            pos = i;
-            length = d[i];
+    free(subsequence);
+    free(indexes);
+    return DEFAULT;
+}
+
+int ceil_index(int *subsequence, int l, int r, int key){
+    if (!subsequence) {
+        return NULL_PTR;
+    }
+
+    if (r <= l) {
+        return WRONG_ARGS;
+    }
+
+    int mid = 0;
+    int left = l;
+    int right = r;
+    int ceil_index = 0;
+    int ceil_index_found = 0;
+
+    for (mid = (left + right) / 2; left <= right && (ceil_index_found == 0); mid = (left + right) / 2) {
+        if (subsequence[mid] > key) {
+            right = mid - 1;
+        }
+        else if (subsequence[mid] == key) {
+            ceil_index = mid;
+            ceil_index_found = 1;
+        }
+        else if (mid + 1 <= right && subsequence[mid + 1] >= key) {
+            subsequence[mid + 1] = key;
+            ceil_index = mid + 1;
+            ceil_index_found = 1;
+        } else {
+            left = mid + 1;
         }
     }
 
-    *size_dest = 0;
-    int tmp = pos;
-    while (tmp != -1) {
-        tmp = prev[tmp];
-        (*size_dest)++;
+    if (ceil_index_found == 0) {
+        if (mid == left) {
+            subsequence[mid] = key;
+            ceil_index = mid;
+        }
+        else {
+            subsequence[mid + 1] = key;
+            ceil_index = mid + 1;
+        }
     }
 
-    (*dest) = (int*)malloc((*size_dest) * sizeof(int));
-    if (!(*dest)) {
-        free(d);
-        free(prev);
-        return ERR_MEM_ALLOC;
-    }
-
-    int i = 0;
-    while (pos != -1) {
-        (*dest)[i++] = src[pos];
-        pos = prev[pos];
-    }
-
-    for (int i = 0; i < ((*size_dest) / 2); i++) {
-        tmp = (*dest)[i];
-        (*dest)[i] = (*dest)[(*size_dest) - 1 - i];
-        (*dest)[(*size_dest) - 1 - i] = tmp;
-    }
-
-    free(d);
-    free(prev);
-    return DEFAULT;
+    return ceil_index;
 }
