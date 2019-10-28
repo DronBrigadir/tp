7c4c5345ef2949533c8bb0542739342690ac29e0
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-10-28 13:27:25 +0300

    now the admin page shows the model of the ask application

--- ASKZUB/ask/admin.py
+++ ASKZUB/ask/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Author
+from .models import Question
+from .models import Answer
+from .models import Tag
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Tag)
