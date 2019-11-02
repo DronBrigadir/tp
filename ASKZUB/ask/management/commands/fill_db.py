3adbf4452666c1a6d74211f8c0e73bafad62a07b
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-11-02 16:21:17 +0300

    add data for new models

--- ASKZUB/ask/management/commands/fill_db.py
+++ ASKZUB/ask/management/commands/fill_db.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from django.db.models import Sum
 from faker import Faker
 from random import choice
 from pytz import timezone
 
-from ask.models import Author, Question, Answer, Tag
+from ask.models import Author, Question, Answer, Tag, QuestionVote, AnswerVote
 
 f = Faker()
 
@@ -22,8 +23,7 @@ class Command(BaseCommand):
                                     password=f.password(),
                                     email=f.free_email())
             u.save()
-            a = Author.objects.create(user=u,
-                                      rating=f.random_int(min=-100, max=100))
+            a = Author.objects.create(user=u)
             a.save()
 
     def question_generator(self):
@@ -34,7 +34,6 @@ class Command(BaseCommand):
                 q = Question.objects.create(author=a,
                                             title=f.text(16),
                                             content=f.text(256),
-                                            votes=f.random_int(min=-100, max=100),
                                             creation_time=f.date_time_this_year(tzinfo=moscow_tz))
                 q.save()
 
@@ -46,7 +45,6 @@ class Command(BaseCommand):
             for _ in range(0, self.ANSWERS_COUNT_FOR_ONE_QUESTION):
                 a = Answer.objects.create(author=choice(authors),
                                           question=q,
-                                          votes=f.random_int(min=-100, max=100),
                                           content=f.text(256),
                                           creation_time=f.date_time_between(start_date=q.creation_time,
                                                                             end_date='+30d',
@@ -61,11 +59,45 @@ class Command(BaseCommand):
                 t.question.add(choice(questions))
             t.save()
 
+    def votes_generator(self):
+        authors = Author.objects.all()
+        questions = Question.objects.all()
+        answers = Answer.objects.all()
+
+        for u in authors:
+            for q in questions:
+                qv = QuestionVote.objects.create(author=u,
+                                                 question=q,
+                                                 value=f.random_int(min=0, max=1))
+                qv.save()
+
+            for a in answers:
+                av = AnswerVote.objects.create(author=u,
+                                               answer=a,
+                                               value=f.random_int(min=0, max=1))
+                av.save()
+
+        for q in questions:
+            likes = QuestionVote.objects.filter(question=q, value=1).count()
+            dislikes = QuestionVote.objects.filter(question=q, value=0).count()
+            Question.objects.filter(pk=q.pk).update(rating=likes - dislikes)
+
+        for a in answers:
+            likes = AnswerVote.objects.filter(answer=a, value=1).count()
+            dislikes = AnswerVote.objects.filter(answer=a, value=0).count()
+            Answer.objects.filter(pk=a.pk).update(rating=likes - dislikes)
+
+        for a in authors:
+            qvotes = Question.objects.filter(author=a).aggregate(Sum('rating'))
+            avotes = Answer.objects.filter(author=a).aggregate(Sum('rating'))
+            Author.objects.filter(pk=a.pk).update(rating=qvotes['rating__sum'] + avotes['rating__sum'])
+
     def generator(self):
         self.authors_generator()
         self.question_generator()
         self.answers_generator()
         self.tags_generator()
+        self.votes_generator()
 
     def handle(self, *args, **options):
         self.generator()
