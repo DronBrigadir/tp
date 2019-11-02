c7ac55e0f29d3974f83dea61c416945ba6033f00
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-11-02 16:21:01 +0300

    add new models to admin page

--- ASKZUB/ask/admin.py
+++ ASKZUB/ask/admin.py
@@ -3,8 +3,12 @@ from .models import Author
 from .models import Question
 from .models import Answer
 from .models import Tag
+from .models import AnswerVote
+from .models import QuestionVote
 
 admin.site.register(Author)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Tag)
+admin.site.register(QuestionVote)
+admin.site.register(AnswerVote)
