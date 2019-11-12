a5041fef7d3125c30e5e87eecfb0f0ce5775fae5
Dronbrigadir <zubkov.andrey.99@mail.ru>
2019-11-13 01:01:46 +0300

    fill_db: some fix and optimization of function

--- ASKZUB/ask/management/commands/fill_db.py
+++ ASKZUB/ask/management/commands/fill_db.py
@@ -13,9 +13,10 @@ f = Faker()
 class Command(BaseCommand):
 
     USERS_COUNT = 3
-    TAGS_COUNT = 3
+    TAGS_COUNT = 10
     QUESTIONS_COUNT_FOR_ONE_USER = 3
     ANSWERS_COUNT_FOR_ONE_QUESTION = 3
+    TAGS_COUNT_FOR_ONE_QUESTION = 3
 
     def authors_generator(self):
         for _ in range(0, self.USERS_COUNT):
@@ -27,70 +28,80 @@ class Command(BaseCommand):
             a.save()
 
     def question_generator(self):
-        authors = Author.objects.all()
+        author_ids = list(Author.objects.values_list('id', flat=True))
         moscow_tz = timezone('Europe/Moscow')
-        for a in authors:
+        for a_id in author_ids:
             for _ in range(0, self.QUESTIONS_COUNT_FOR_ONE_USER):
-                q = Question.objects.create(author=a,
+                q = Question.objects.create(author_id=a_id,
                                             title=f.text(16),
                                             content=f.text(256),
                                             creation_time=f.date_time_this_year(tzinfo=moscow_tz))
                 q.save()
 
     def answers_generator(self):
-        authors = Author.objects.all()
-        questions = Question.objects.all()
+        author_ids = list(Author.objects.values_list('id', flat=True))
+        question_ids = list(Question.objects.values_list('id', flat=True))
         moscow_tz = timezone('Europe/Moscow')
-        for q in questions:
+        for q_id in question_ids:
             for _ in range(0, self.ANSWERS_COUNT_FOR_ONE_QUESTION):
-                a = Answer.objects.create(author=choice(authors),
-                                          question=q,
+                a = Answer.objects.create(author_id=choice(author_ids),
+                                          question_id=q_id,
                                           content=f.text(256),
-                                          creation_time=f.date_time_between(start_date=q.creation_time,
-                                                                            end_date='+30d',
-                                                                            tzinfo=moscow_tz))
+                                          creation_time=
+                                          f.date_time_between(start_date=Question.objects.get(pk=q_id).creation_time,
+                                                              end_date='+30d',
+                                                              tzinfo=moscow_tz))
                 a.save()
 
     def tags_generator(self):
-        questions = Question.objects.all()
         for _ in range(0, self.TAGS_COUNT):
-            t = Tag.objects.create(name=f.color_name())
-            for _ in range(0, self.QUESTIONS_COUNT_FOR_ONE_USER):
-                t.question.add(choice(questions))
+            name = f.color_name()
+            while Tag.objects.filter(name=name).exists():
+                name = f.color_name()
+
+            t = Tag.objects.create(name=name)
             t.save()
 
+        question_ids = list(Question.objects.values_list('id', flat=True))
+        tag_ids = list(Tag.objects.values_list('id', flat=True))
+        for q_id in question_ids:
+            for _ in range(0, self.TAGS_COUNT_FOR_ONE_QUESTION):
+                Question.objects.get(pk=q_id).tag_set.add(Tag.objects.get(pk=choice(tag_ids)))
+
     def votes_generator(self):
-        authors = Author.objects.all()
-        questions = Question.objects.all()
-        answers = Answer.objects.all()
+        author_ids = list(Author.objects.values_list('id', flat=True))
+        question_ids = list(Question.objects.values_list('id', flat=True))
+        answer_ids = list(Answer.objects.values_list('id', flat=True))
 
-        for u in authors:
-            for q in questions:
-                qv = QuestionVote.objects.create(author=u,
-                                                 question=q,
-                                                 value=f.random_int(min=0, max=1))
-                qv.save()
+        for author_id in author_ids:
+            for q_id in question_ids:
+                if Question.objects.get(pk=q_id).author_id != author_id:
+                    qv = QuestionVote.objects.create(author_id=author_id,
+                                                     question_id=q_id,
+                                                     value=f.random_int(min=0, max=1))
+                    qv.save()
 
-            for a in answers:
-                av = AnswerVote.objects.create(author=u,
-                                               answer=a,
-                                               value=f.random_int(min=0, max=1))
-                av.save()
+            for answer_id in answer_ids:
+                if Answer.objects.get(pk=answer_id).author_id != author_id:
+                    av = AnswerVote.objects.create(author_id=author_id,
+                                                   answer_id=answer_id,
+                                                   value=f.random_int(min=0, max=1))
+                    av.save()
 
-        for q in questions:
-            likes = QuestionVote.objects.filter(question=q, value=1).count()
-            dislikes = QuestionVote.objects.filter(question=q, value=0).count()
-            Question.objects.filter(pk=q.pk).update(rating=likes - dislikes)
+        for q_id in question_ids:
+            likes = QuestionVote.objects.filter(question_id=q_id, value=1).count()
+            dislikes = QuestionVote.objects.filter(question_id=q_id, value=0).count()
+            Question.objects.filter(pk=q_id).update(rating=likes - dislikes)
 
-        for a in answers:
-            likes = AnswerVote.objects.filter(answer=a, value=1).count()
-            dislikes = AnswerVote.objects.filter(answer=a, value=0).count()
-            Answer.objects.filter(pk=a.pk).update(rating=likes - dislikes)
+        for answer_id in answer_ids:
+            likes = AnswerVote.objects.filter(answer_id=answer_id, value=1).count()
+            dislikes = AnswerVote.objects.filter(answer_id=answer_id, value=0).count()
+            Answer.objects.filter(pk=answer_id).update(rating=likes - dislikes)
 
-        for a in authors:
-            qvotes = Question.objects.filter(author=a).aggregate(Sum('rating'))
-            avotes = Answer.objects.filter(author=a).aggregate(Sum('rating'))
-            Author.objects.filter(pk=a.pk).update(rating=qvotes['rating__sum'] + avotes['rating__sum'])
+        for author_id in author_ids:
+            qvotes = Question.objects.filter(author_id=author_id).aggregate(Sum('rating'))
+            avotes = Answer.objects.filter(author_id=author_id).aggregate(Sum('rating'))
+            Author.objects.filter(pk=author_id).update(rating=qvotes['rating__sum'] + avotes['rating__sum'])
 
     def generator(self):
         self.authors_generator()
